a4df8f2a77745197b7deca28337ad2286b04bce7
felipesantos2 <santospinheiro627@gmail.com>
2024-11-28 09:22:31 -0300

    add plugin boilerplate

---
+++ seeddir.php
@@ -0,0 +1,20 @@
+<?php
+
+
+/**
+ *
+ * @author            Felipe Pinheiro
+ * @copyright         2024 Felipe Pinheiro
+ * @license           https://unlicense.org
+ *
+ *  Plugin Name: Seeddir - mail tester
+ *  Author: Felipe Pinheiro
+ *  Author URI: https://github.com/felipesantos2
+ *  Version: 1.0.0
+ *  Licence:
+ *  Text Domain: seeddir
+ *
+ */
+
+
+define('BASE_PATH', __DIR__);
