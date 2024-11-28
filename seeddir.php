128d13c4a456346d47c2a9e82d32ed84be265755
felipesantos2 <santospinheiro627@gmail.com>
2024-11-28 10:45:46 -0300

    Update seeddir.php

--- seeddir.php
+++ seeddir.php
@@ -3,11 +3,11 @@
 
 /**
  *
- * @author            Felipe Pinheiro
- * @copyright         2024 Felipe Pinheiro
- * @license           https://unlicense.org
+ *  @author            Felipe Pinheiro
+ *  @copyright         2024 Felipe Pinheiro
+ *  @license           https://unlicense.org
  *
- *  Plugin Name: Seeddir - mail tester
+ *  Plugin Name: Seeddir - Mail Tester
  *  Author: Felipe Pinheiro
  *  Author URI: https://github.com/felipesantos2
  *  Version: 1.0.0
@@ -17,6 +17,6 @@
  */
 
 
-defined(ABSPATH) || throw new Exception('Constane não definida');
+defined(ABSPATH) || throw new Exception('Constante ' . ABSPATH . 'não definida');
 
 define('BASE_PATH', __DIR__);
