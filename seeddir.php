38dc8b213605f8b26fd95d612613410364c02c3c
felipesantos2 <santospinheiro627@gmail.com>
2024-11-28 11:22:57 -0300

    add files and more

--- seeddir.php
+++ seeddir.php
@@ -14,9 +14,36 @@
  *  Licence:
  *  Text Domain: seeddir
  *
+ *  Description: Email tester
+ *
  */
 
 
-defined(ABSPATH) || throw new Exception('Constante ' . ABSPATH . 'não definida');
+if (!defined('ABSPATH')) {
+    throw new Exception('Constante ' . ABSPATH . ' não definida');
+}
 
 define('BASE_PATH', __DIR__);
+
+
+add_action('admin_menu', 'seeddir_add_admin_menu');
+
+function seeddir_add_admin_menu()
+{
+    add_menu_page(
+        'Include Text',     // page title
+        'Enviar Emails',     // menu title
+        'manage_options',   // capability
+        'include-text',     // menu slug
+        function () {
+            
+            global $title;
+
+            echo '<div class="wrap">';
+
+                echo "<h1>$title</h1>";
+
+            echo '</div>';
+        } // callback function
+    );
+}
