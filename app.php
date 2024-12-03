6bacc1c0c0e5aea841192eb577a7af6caff8995c
felipesantos2 <santospinheiro627@gmail.com>
2024-12-03 08:29:12 -0300

    add new page

--- app.php
+++ app.php
@@ -10,30 +10,60 @@ add_action('admin_enqueue_scripts', function () {
     );
 });
 
+
+function seeddir_emails_page()
+{
+    if (!current_user_can('manage_options')) {
+        return;
+    }
+
+    global $title;
+
+    echo '<div class="wrap">';
+
+        echo "<h1>$title</h1>";
+
+        require __DIR__ . '/admin/list-emails' . '.php';
+
+    echo '</div>';
+}
+
+
+function seeddir_post_type_order_page()
+{
+    if (!current_user_can('manage_options')) {
+        return;
+    }
+
+    global $title;
+
+    echo '<div class="wrap">';
+
+        // echo "<h1>$title</h1>";
+
+        // require __DIR__ . '/admin/list-emails' . '.php';
+
+    echo '</div>';
+}
+
 add_action('admin_menu', 'seeddir_add_admin_menu');
+
 
 function seeddir_add_admin_menu()
 {
     add_menu_page(
-        'Teste seus E-mails',     // page title
-        'E-mails Send',     // menu title
-        'manage_options',   // capability
-        'emails-send',     // menu slug
-        function () {
+        'Teste seus E-mails',       // page title
+        'E-mails Send',             // menu title
+        'manage_options',           // capability
+        'emails-send',              // menu slug
+        'seeddir_emails_page'       // calback function
+    );
 
-            if (!current_user_can('manage_options')) {
-                return;
-            }
-
-            global $title;
-
-            echo '<div class="wrap">';
-
-            echo "<h1>$title</h1>";
-
-            require __DIR__ . '/admin/list-emails' . '.php';
-
-            echo '</div>';
-        } // callback function
+    add_menu_page(
+        'Order Posts',
+        'Ordene os Posts',
+        'manage_options',
+        'posts-order',
+        'seeddir_post_type_order_page'
     );
 }
